0bdb01a13863a5048422a84ed7158ddb1d54ffa5
Damola090 <96941448+Damola090@users.noreply.github.com>
2024-03-15 14:47:13 +0100

    web_cors_fix

--- src/socketMain.js
+++ src/socketMain.js
@@ -32,9 +32,7 @@ const io = socketio(server, {
   cors: {
     // origin: ["http://127.0.0.1:3001"],
     origin: [
-      "https://fieldskawd.ctng.biz/",
-      "http://192.168.218.145:3001",
-      "http://127.0.0.1:3001",
+      "https://fieldskawd.ctng.biz/", "http://127.0.0.1:3001",
     ],
     credentials: true,
     methods: ["GET", "POST"],
