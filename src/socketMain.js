5a780a259ed4c5cca078711853644f173a80bae1
Damola090 <96941448+Damola090@users.noreply.github.com>
2024-03-15 15:44:16 +0100

    commit

--- src/socketMain.js
+++ src/socketMain.js
@@ -31,18 +31,17 @@ const server = http.createServer(app);
 const io = socketio(server, {
   cors: {
     // origin: ["http://127.0.0.1:3001"],
-    origin: [
-      "http://127.0.0.1:3001",
-    ],
-    credentials: true,
-    methods: ["GET", "POST"],
-    transports: ["websocket", "polling"],
+    origin: ["http://192.168.218.145:3001", "http://127.0.0.1:3001"],
+    // credentials: true,
+    // methods: ["GET", "POST"],
+    // transports: ["websocket", "polling"],
   },
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // "http://127.0.0.1:3001"
+
 
 console.log(PORT, "test");
 
